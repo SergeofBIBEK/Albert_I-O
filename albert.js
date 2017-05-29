cbd23c4aa2490d892e38d159ee828fefd8370e66
Brandon Gates <brandon.gates@bbdo.com>
2017-05-28 20:46:51 -0400

    first deploy

---
+++ albert.js
@@ -0,0 +1,203 @@
+var db;
+window.onload = dateStuff();
+
+
+function dateStuff()
+{
+
+}
+
+function signedInHandler()
+{
+    document.getElementById("firebaseui-auth-container").style.display = "none";
+
+    db = firebase.database().ref(currentUser.uid);
+
+    db.child("input").orderByChild("time").limitToLast(5).on('value', function(snapshot){
+        //firstRow
+
+        var newHTML = "<div class='row'><div class='cell'>Item</div><div class='cell'>Amount</div><div class='cell'>Time</div><div class='cell'>Delete</div></div>";
+
+        snapshot.forEach(function(entry){
+            //newRow
+            newHTML += "<div class='row'>";
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + " oz" + "</div>";
+            newHTML += "<div class='cell'>" + new Date(entry.val().time).toLocaleString() + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"input|" + entry.key + "\")'>" +
+                "</div>";
+
+            //endRow
+            newHTML += "</div>";
+        });
+
+        document.getElementById("inputTable").innerHTML = newHTML;
+    });
+
+    db.child("output").orderByChild("time").limitToLast(5).on('value', function(snapshot){
+        //firstRow
+
+        var newHTML = "<div class='row'><div class='cell'>Item</div><div class='cell'>Amount</div><div class='cell'>Time</div><div class='cell'>Delete</div></div>";
+
+        snapshot.forEach(function(entry){
+            //newRow
+            newHTML += "<div class='row'>";
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + "</div>";
+            newHTML += "<div class='cell'>" + new Date(entry.val().time).toLocaleString() + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"output|" + entry.key + "\")'>" +
+                "</div>";
+
+            //endRow
+            newHTML += "</div>";
+        });
+
+        document.getElementById("outputTable").innerHTML = newHTML;
+    });
+    
+    db.child("input").orderByChild("time").on('value', function(snapshot){
+        //firstRow
+
+        var newHTML = "<div class='row'><div class='cell'>Item</div><div class='cell'>Amount</div><div class='cell'>Time</div><div class='cell'>Delete</div></div>";
+
+        snapshot.forEach(function(entry){
+            //newRow
+            newHTML += "<div class='row'>";
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + " oz" + "</div>";
+            newHTML += "<div class='cell'>" + new Date(entry.val().time).toLocaleString() + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"input|" + entry.key + "\")'>" +
+                "</div>";
+
+            //endRow
+            newHTML += "</div>";
+        });
+
+        document.getElementById("inputTableFull").innerHTML = newHTML;
+    });
+
+    db.child("output").orderByChild("time").on('value', function(snapshot){
+        //firstRow
+
+        var newHTML = "<div class='row'><div class='cell'>Item</div><div class='cell'>Amount</div><div class='cell'>Time</div><div class='cell'>Delete</div></div>";
+
+        snapshot.forEach(function(entry){
+            //newRow
+            newHTML += "<div class='row'>";
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + "</div>";
+            newHTML += "<div class='cell'>" + new Date(entry.val().time).toLocaleString() + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"output|" + entry.key + "\")'>" +
+                "</div>";
+
+            //endRow
+            newHTML += "</div>";
+        });
+
+        document.getElementById("outputTableFull").innerHTML = newHTML;
+    });
+    
+}
+
+function signedOutHandler()
+{
+
+}
+
+function showInput()
+{
+    document.getElementById("inputModal").removeAttribute("style");
+        document.getElementById("inputTable").style.display = "none";
+    document.getElementById("outputTable").style.display = "none";
+    var currentTime = new Date();
+    document.getElementById("inputTime").value = currentTime.getFullYear()+"-"+zeroPadded(currentTime.getMonth() + 1)+"-"+zeroPadded(currentTime.getDate())+"T"+zeroPadded(currentTime.getHours())+":"+zeroPadded(currentTime.getMinutes())+":"+zeroPadded(currentTime.getSeconds());
+}
+
+function showOutput()
+{
+    document.getElementById("outputModal").removeAttribute("style");
+        document.getElementById("inputTable").style.display = "none";
+    document.getElementById("outputTable").style.display = "none";
+    var currentTime = new Date();
+    document.getElementById("outputTime").value = currentTime.getFullYear()+"-"+zeroPadded(currentTime.getMonth() + 1)+"-"+zeroPadded(currentTime.getDate())+"T"+zeroPadded(currentTime.getHours())+":"+zeroPadded(currentTime.getMinutes())+":"+zeroPadded(currentTime.getSeconds());
+}
+
+function saveInput()
+{
+    var item = document.getElementById("inputItem").value;
+    var quantity = document.getElementById("inputQuantity").value;
+    var time = document.getElementById("inputTime").value;
+
+    db.child("input").push({
+        item: item,
+        quantity: quantity,
+        time: time
+    });
+
+    closeModal();
+}
+
+function saveOutput()
+{
+    var item = document.getElementById("outputItem").value;
+    var quantity = document.getElementById("outputQuantity").value;
+    var time = document.getElementById("outputTime").value;
+
+    db.child("output").push({
+        item: item,
+        quantity: quantity,
+        time: time
+    });
+
+    closeModal();
+}
+
+function closeModal()
+{
+    var modals = document.getElementsByClassName("modal");
+
+    for (var i = 0; i < modals.length; i++)
+    {
+        modals[i].style.display = "none";
+    }
+        document.getElementById("inputTable").removeAttribute("style");
+    document.getElementById("outputTable").removeAttribute("style");
+}
+
+function deleteEntry(data)
+{
+    db.child(data.split("|")[0]).child(data.split("|")[1]).remove();
+}
+
+function showFullInput()
+{
+    document.getElementById("fullInput").removeAttribute("style");
+    document.getElementById("inputTable").style.display = "none";
+    document.getElementById("outputTable").style.display = "none";
+}
+
+function showFullOutput()
+{
+    document.getElementById("fullOutput").removeAttribute("style");
+        document.getElementById("inputTable").style.display = "none";
+    document.getElementById("outputTable").style.display = "none";
+}
+
+function zeroPadded(val) {
+    if (val >= 10)
+        return val;
+    else
+        return '0' + val;
+}
---
+++ fbli.js
@@ -0,0 +1,63 @@
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: '',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+/*        firebase.auth.EmailAuthProvider.PROVIDER_ID*/
+    ],
+    // Terms of service url.
+    tosUrl: ''
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+initUser = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            currentUser.signInStatus = true;
+            currentUser.displayName = user.displayName;
+            currentUser.email = user.email;
+            currentUser.emailVerified = user.emailVerified;
+            currentUser.photoURL = user.photoURL;
+            if (currentUser.photoURL == null)
+            {
+                currentUser.photoURL = 'https://openclipart.org/download/237991/users81.svg';
+            }
+            currentUser.uid = user.uid;
+            currentUser.providerData = user.providerData;
+            signedInHandler();
+        } else {
+            // User is signed out.
+            currentUser.signInStatus = false;
+            currentUser.displayName = "";
+            currentUser.email = "";
+            currentUser.emailVerified = "";
+            currentUser.photoURL = "";
+            currentUser.uid = "";
+            currentUser.providerData = "";
+            signedOutHandler();
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
+
+window.addEventListener('load', function() {
+    initUser();
+});
+
+var currentUser = {
+    signInStatus:"",
+    displayName:"",
+    email:"",
+    emailVerified:"",
+    photoURL:"",
+    uid:"",
+    providerData:"",
+}
