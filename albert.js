398fcb6049eb44c70434ad72b40a4c717b0d9b11
Brandon Gates <brandon.gates@bbdo.com>
2017-05-28 22:53:16 -0400

    try again

--- albert.js
+++ albert.js
@@ -143,7 +143,7 @@ function saveInput()
     db.child("input").push({
         item: item,
         quantity: quantity,
-        time: time.valueOf()
+        time: time.toISOString();
     });
 
     closeModal();
