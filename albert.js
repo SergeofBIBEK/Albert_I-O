bbd0c66f9e71e8a4ca303f0de2de416dde1d960f
Brandon Gates <brandon.gates@bbdo.com>
2017-06-10 09:50:36 -0400

    poop viewer

--- albert.js
+++ albert.js
@@ -21,15 +21,15 @@ function signedInHandler()
 
         snapshot.forEach(function(entry){
             var myArray = entry.val().time.split("T");
-            var currentDate = new Date().toDateString();
-            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toDateString();
-                        
+            var currentDate = new Date().toLocaleDateString();
+            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toLocaleDateString();
+
             if (currentDate == itemDate)
             {
 
                 var hours = myArray[1].slice(0, 2);
                 var ampm = hours >= 12 ? "PM" : "AM";
-                
+
                 //newRow
                 newHTML += "<div class='row'>";
 
@@ -57,15 +57,15 @@ function signedInHandler()
 
         snapshot.forEach(function(entry){
             var myArray = entry.val().time.split("T");
-            var currentDate = new Date().toDateString();
-            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toDateString();
-                        
+            var currentDate = new Date().toLocaleDateString();
+            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toLocaleDateString();
+
             if (currentDate == itemDate)
             {
 
                 var hours = myArray[1].slice(0, 2);
                 var ampm = hours >= 12 ? "PM" : "AM";
-                
+
                 //newRow
                 newHTML += "<div class='row'>";
 
@@ -93,25 +93,25 @@ function signedInHandler()
 
         snapshot.forEach(function(entry){
             var myArray = entry.val().time.split("T");
-            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toDateString();
-                        
-                var hours = myArray[1].slice(0, 2);
-                var ampm = hours >= 12 ? "PM" : "AM";
-                
-                //newRow
-                newHTML += "<div class='row'>";
+            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toLocaleDateString();
 
-                var prettyDate = itemDate + ", " + (hours % 12 || 12) + myArray[1].slice(2, 5) + " " + ampm;
+            var hours = myArray[1].slice(0, 2);
+            var ampm = hours >= 12 ? "PM" : "AM";
 
-                //cells
-                newHTML += "<div class='cell'>" + entry.val().item + "</div>";
-                newHTML += "<div class='cell'>" + entry.val().quantity + " oz" + "</div>";
-                newHTML += "<div class='cell'>" + prettyDate + "</div>";
-                newHTML += "<div class='cell'>" + 
-                    "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"input|" + entry.key + "\")'>" +
-                    "</div>";
-                //endRow
-                newHTML += "</div>";
+            //newRow
+            newHTML += "<div class='row'>";
+
+            var prettyDate = itemDate + ", " + (hours % 12 || 12) + myArray[1].slice(2, 5) + " " + ampm;
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + " oz" + "</div>";
+            newHTML += "<div class='cell'>" + prettyDate + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"input|" + entry.key + "\")'>" +
+                "</div>";
+            //endRow
+            newHTML += "</div>";
         });
 
         document.getElementById("inputTableFull").innerHTML = newHTML;
@@ -124,12 +124,47 @@ function signedInHandler()
 
         snapshot.forEach(function(entry){
             var myArray = entry.val().time.split("T");
-            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toDateString();
-                        
+            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toLocaleDateString();
+
+
+            var hours = myArray[1].slice(0, 2);
+            var ampm = hours >= 12 ? "PM" : "AM";
+
+            //newRow
+            newHTML += "<div class='row'>";
+
+            var prettyDate = itemDate + ", " + (hours % 12 || 12) + myArray[1].slice(2, 5) + " " + ampm;
+
+            //cells
+            newHTML += "<div class='cell'>" + entry.val().item + "</div>";
+            newHTML += "<div class='cell'>" + entry.val().quantity + "</div>";
+            newHTML += "<div class='cell'>" + prettyDate + "</div>";
+            newHTML += "<div class='cell'>" + 
+                "<input type='button' value='x' class='deleteButton' onclick='deleteEntry(\"input|" + entry.key + "\")'>" +
+                "</div>";
+            //endRow
+            newHTML += "</div>";
+        });
+
+        document.getElementById("outputTableFull").innerHTML = newHTML;
+    });
+
+    db.child("output").orderByChild("time").on('value', function(snapshot){
+        //firstRow
+
+        var newHTML = "<div class='row'><div class='cell'>Item</div><div class='cell'>Amount</div><div class='cell'>Time</div><div class='cell'>Delete</div></div>";
+
+        snapshot.forEach(function(entry){
+            var myArray = entry.val().time.split("T");
+            var itemDate = new Date(myArray[0].replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$2/$3/$1")).toLocaleDateString();
+
+
+            if (entry.val().item == "Poop")
+            {
 
                 var hours = myArray[1].slice(0, 2);
                 var ampm = hours >= 12 ? "PM" : "AM";
-                
+
                 //newRow
                 newHTML += "<div class='row'>";
 
@@ -144,9 +179,10 @@ function signedInHandler()
                     "</div>";
                 //endRow
                 newHTML += "</div>";
+            }
         });
 
-        document.getElementById("outputTableFull").innerHTML = newHTML;
+        document.getElementById("outputTableNumber2").innerHTML = newHTML;
     });
 
 }
@@ -238,6 +274,14 @@ function showFullInput()
 function showFullOutput()
 {
     document.getElementById("fullOutput").removeAttribute("style");
+    document.getElementById("inputTable").style.display = "none";
+    document.getElementById("outputTable").style.display = "none";
+}
+
+
+function showNumber2Output()
+{
+    document.getElementById("number2Output").removeAttribute("style");
     document.getElementById("inputTable").style.display = "none";
     document.getElementById("outputTable").style.display = "none";
 }
